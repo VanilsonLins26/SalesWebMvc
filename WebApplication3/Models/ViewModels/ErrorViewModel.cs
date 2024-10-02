e8d78b998408a072cfdeba8fe6b3aeeb09f3f0d8
vanilson <vanilson.liins@gmail.com>
2024-10-02 14:13:09 -0300

    Primeiro commit

---
+++ WebApplication3/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication3.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
